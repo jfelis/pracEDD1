d02789bdb0a90be513f8bc282e39058504d8c3ef
Juanjo Felis <jfelis@ieseljust.com>
2018-12-15 20:46:10 +0100

    Codi inicial

---
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repoedd1;
+
+/**
+ *
+ * @author espe
+ */
+public class RepoEDD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        	System.out.println("14-12-2018");
+
+
+    }
+    
+}
